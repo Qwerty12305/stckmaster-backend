a89e928e0296490b50a5df3ceea79a87c91e8a5a
Qwerty12305 <jodhasingh345646@gmail.com>
2025-08-07 03:32:44 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   mobile: { type: String, unique: true },
   password: String,
   userId: { type: String, unique: true }, // 6-digit ID (e.g., 245612)
-  status: { type: String, default: "pending" },
+  status: { type: String, default: "active" },
   referralCode: { type: String },      // Add this
   referredBy: { type: String, default: null }  // Add this
 });
