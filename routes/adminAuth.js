80b9ca557b6ca4ae1eb718ce327e310b4ad2e3fc
Qwerty12305 <jodhasingh345646@gmail.com>
2026-06-21 14:52:26 +0530

    update

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const Admin = require('../models/Admin');
-const Admin = require('../models/Adminxys');
 const bcrypt = require('bcryptjs');
 
 router.post('/login', async (req, res) => {
@@ -28,7 +27,7 @@ router.post('/login', async (req, res) => {
       message: 'Login successful',
       adminId: admin._id,
       name: admin.name,
-      // optionally send a token or session ID if implemented jay mata di
+      // optionally send a token or session ID if implemented
     });
   } catch (err) {
     console.error('Login error:', err);
